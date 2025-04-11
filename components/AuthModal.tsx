1ad9a9c4fb02a486b32904de80ad7efebd7c30bf
Dharun R <dharunamikaze@gmail.com>
2025-04-11 13:59:16 +0530

    Removed Convex

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,9 +1,6 @@
 'use client';
 import React, { useState } from 'react';
 import { Dialog } from "../components/ui/Dialog";
-// import { Button } from "@/components/ui/button";
-// import { useMutation } from "convex/react";
-// import { api } from "@/convex/_generated/api";
 
 export const AuthModal = () => {
     const [open, setOpen] = useState(false);
--- convex/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    providers: [
-      {
-        domain: "https://upright-man-27.clerk.accounts.dev",
-        applicationID: "convex",
-      },
-    ]
-  };
