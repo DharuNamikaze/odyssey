1b3bfbcede4a06b8b8fe68d7ec862fe910e98a80
Dharun R <dharunamikaze@gmail.com>
2025-04-21 14:02:54 +0530

    singout logic written

--- components/ui/sideBar.tsx
+++ components/ui/sideBar.tsx
@@ -1,12 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, {  useEffect, useState } from 'react'
 import Link from 'next/link'
-import { IconEdit, IconFidgetSpinner } from '@tabler/icons-react'
+import { IconEdit, IconFidgetSpinner, IconLogout2 } from '@tabler/icons-react'
 import { onAuthStateChanged, User } from 'firebase/auth';
-import { getAuth } from "../../lib/firebase";
+import { getAuth, signOut } from "../../lib/firebase";
 
 export function Sidebar() {
     const [bro, setBro] = useState<User | null>(null)
     const [loading, setLoading] = useState<boolean>(true)
+
+    const handleSignOut = () => {
+        setLoading(true);
+        const auth = getAuth();
+        signOut(auth);
+        console.log(signOut(auth));
+        setLoading(false);
+    }
     useEffect(() => {
         const fetchBro = () => {
             try {
@@ -31,18 +39,18 @@ export function Sidebar() {
     );
 
     return (
-        <nav className="text-black bg-amber-100 p-2 flex flex-col h-screen w-[25vh] rounded-lg z-50">
+        <nav className="text-black bg-amber-100 p-2 flex flex-col h-screen w-[30vh] rounded-lg z-50">
             {loading && <LoadingModal />}
             <span className="border-2 border-blue-600 rounded-lg p-1 flex justify-between "> <strong>{bro?.displayName}</strong> <Link href="" className="">
                 <IconEdit />
             </Link> </span>
-            <menu className="border-2 border-blue-600 rounded-lg flex flex-col justify-between space-y-5">
-                Menu
+            <menu className="border-2 border-blue-600 p-0.5 rounded-lg flex flex-col justify-between space-y-5">
+                <span>Menu</span>
                 <li >Search</li>
                 <li>Inbox</li>
                 <li>Odyssey AI</li>
             </menu>
-            <menu className="flex flex-col">
+            <menu className="flex flex-col flex-1">
                 Favorites
                 <li >Projects</li>
                 <li></li>
@@ -50,6 +58,7 @@ export function Sidebar() {
                 <li></li>
                 <li></li>
             </menu>
+            <button className='flex gap-5 p-2 cursor-pointer' onClick={handleSignOut}><IconLogout2 /> Logout </button>
         </nav>
     )
 }
