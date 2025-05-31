c346f6478819363ecaa3e3f89a7e5afbb3b9378a
DharuNamikaze <dharunamikaze@gmail.com>
2025-05-31 13:59:00 +0530

    Modified Gitignore

---
+++ lib/firebaseAdmin.ts
@@ -0,0 +1,21 @@
+import { initializeApp, cert, getApps, getApp } from 'firebase-admin/app';
+import admin from 'firebase-admin';
+import { getFirestore } from 'firebase-admin/firestore';
+import { getAuth } from 'firebase-admin/auth';
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
+
+if (!admin.apps.length){
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+    })
+}
+const app = !getApps().length ? initializeApp({
+    credential: cert(serviceAccount), 
+})
+: getApp()
+
+const db = getFirestore(app)
+const auth = getAuth(app)
+
+export {auth, db, admin};
