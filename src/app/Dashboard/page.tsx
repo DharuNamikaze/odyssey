2655a28381abdeb1bf84c0799ee23d5191ca8c39
DharuNamikaze <dharunamikaze@gmail.com>
2025-07-02 04:26:54 +0530

    major ui changes

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -40,38 +40,50 @@ const Dashboard = () => {
     //         </div>
     //     </div>
     // );
-    
+
     return (
         <>
             <main className="flex flex-col gap-10 overflow-hidden ">
-                <div className="grid grid-cols-1 gap-6 z-4 mb-10">
-                    <h6 className="text-xl mt-5">Summary</h6>
-                    <div className="flex gap-6 z-5 p-4 rounded-2xl bg-[var(--bggray)] items-center ">
+                <div className="grid grid-cols-1 gap-5 z-4 mb-10 ">
+                    <div >
+                        <h1 className="mt-5 text-4xl font-bold p-0 mb-0">Summary</h1>
+                        <p className="text-neutral-400 mt-1">Welcome back! Here's an overview of your Activites.</p>
+                    </div>
+                    <div className="flex gap-6 z-5 p-4 rounded-2xl bg-gradient-to-tr from-black to-zinc-700 items-center border-[0.2px] border-neutral-700 ">
                         <div className="max-md:grid max-md:grid-cols-1 flex gap-5 items-center text-sm">
-                            <div className="rounded-2xl bg-black p-2 flex flex-col text-base gap-1">Streak <span className="text-xl">1🔥</span></div>
-                            <div className="rounded-2xl bg-black p-2 flex flex-col text-base gap-1">Aura <span className="text-xl">1223🌟</span></div>
-                            <div className="rounded-2xl bg-black p-2 flex flex-col text-base gap-1">Level <span className="text-xl">{Levels[0][0]}</span> </div>
+                            <div className="rounded-2xl bg-gradient-to-br from-black to-neutral-800 flex flex-col py-6 pr-20 pl-5 text-base gap-1 justify-evenly">
+                                <h1 className="font-semibold text-xl">Streak 🔥</h1>
+                                <span className="text-xl pt-3 text-neutral-400">1</span>
+                            </div>
+                            <div className="rounded-2xl bg-gradient-to-br from-black to-neutral-800 py-6 pr-20 pl-5 flex flex-col text-base gap-1">
+                                <h1 className="font-semibold text-xl">Aura 🌟</h1>
+                                <span className="text-xl pt-3 text-neutral-400"> 1223</span>
+                            </div>
+                            <div className="rounded-2xl  bg-gradient-to-br from-black to-neutral-800 py-6 pr-20 pl-5 flex flex-col text-base gap-1 ">
+                                <h1 className="font-semibold text-xl">Level 🚀</h1>
+                                <span className="text-xl pt-3 text-neutral-400">{Levels[0][0]}</span>
+                            </div>
                         </div>
                     </div>
 
-                    <div className="p-8 rounded-2xl bg-[var(--bggray)] grid max-md:grid-cols-1 grid-cols-2 flex-1 space-y-2" >
+                    <div className="p-8 rounded-2xl bg-gradient-to-tr from-black to-zinc-700 grid max-md:grid-cols-1 grid-cols-2 flex-1 space-y-2 border-[0.1px] border-neutral-700" >
                         <div className="space-y-9"><DonutChartCenterText /> <span className="flex justify-center">Habits</span></div>
                         <div><HorizontalBarChart /></div>
 
                         {/* <span className="flex justify-center">Habits</span> */}
                     </div>
 
-                    <div className="p-8 rounded-2xl bg-[var(--bggray)] flex flex-col space-y-3" >
+                    <div className="p-8 rounded-2xl bg-gradient-to-tr from-black to-zinc-700 flex flex-col space-y-3 border-[0.2px] border-neutral-700" >
                         <LineChart />
                         <span className="flex justify-center">Sleep Hours</span>
                     </div>
 
-                    <div className="p-8 rounded-2xl bg-[var(--bggray)] flex flex-col space-y-3" >
+                    <div className="p-8 rounded-2xl bg-gradient-to-tr from-black to-zinc-700 flex flex-col space-y-3 border-[0.2px] border-neutral-700" >
                         <LineCustomChart />
                         <span className="flex justify-center">Emotion Timeline</span>
                     </div>
 
-                    <div className="p-8 rounded-2xl bg-[var(--bggray)] flex flex-col space-y-3 " >
+                    <div className="p-8 rounded-2xl bg-gradient-to-tr from-black to-zinc-700 flex flex-col space-y-3 border-[0.2px] border-neutral-700 " >
                         <BarChartHorizontalLogo />
                         <span className="flex justify-center">Valued Emotion</span>
                     </div>
