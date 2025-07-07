e036707a2daae05745b5c760c910ff548cded3d7
DharuNamikaze <dharunamikaze@gmail.com>
2025-07-07 10:56:11 +0530

    fbadmin fix 1

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -2,25 +2,25 @@ import { initializeApp, cert, getApps, getApp } from 'firebase-admin/app';
 import admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
-const key  = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
-if (!key){
-    throw new Error("NO fucking key is found");
-}
-const serviceAccount = key;
-// JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
 
-if (!admin.apps.length){
+const key = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+if (!key) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT_KEY is not set");
+}
+const serviceAccount = JSON.parse(key);
+
+if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
     })
 }
 const app = !getApps().length ? initializeApp({
-    credential: cert(serviceAccount), 
+    credential: cert(serviceAccount),
 })
-: getApp()
+    : getApp()
 
 const db = getFirestore(app)
 const auth = getAuth(app)
 const adminn = admin;
 
-export {auth, db, adminn};
+export { auth, db, adminn };
